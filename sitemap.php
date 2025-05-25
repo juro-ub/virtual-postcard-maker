39d5284801d34597927f6d64bf0bccd3c74b3d63
jr <julianrost20@gmail.com>
2025-05-25 14:57:19 +0200

    Modify Sitemap

--- sitemap.php
+++ sitemap.php
@@ -3,20 +3,20 @@
 require_once 'init.php';
 header('Content-Type: application/xml; charset=utf-8');
 
-$baseUrl = getBaseUrl();
-$staticPages = [
-    '', // index.php
-    'datenschutz.php',
-    'impressum.php'
-];
+$base = getBaseUrl();
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 echo "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n";
 
-// Statische Seiten
-foreach ($staticPages as $page) {
+$pages = [
+    '',
+    'datenschutz.php',
+    'impressum.php'
+];
+
+foreach ($pages as $p) {
     echo "  <url>\n";
-    echo "    <loc>" . htmlspecialchars($baseUrl . $page) . "</loc>\n";
+    echo "    <loc>" . htmlspecialchars($base . $p) . "</loc>\n";
     echo "    <priority>0.8</priority>\n";
     echo "  </url>\n";
 }
