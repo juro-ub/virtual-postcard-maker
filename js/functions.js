55ce1dd1eb47932723e274398c19e857eef05b4c
jr <julianrost20@gmail.com>
2025-05-25 16:43:09 +0200

    new draggable function

--- js/functions.js
+++ js/functions.js
@@ -10,36 +10,58 @@ function makeDraggable(el, containerSelector = '#postcard') {
     const container = document.querySelector(containerSelector);
     let pos = {x: 0, y: 0, left: 0, top: 0};
 
-    el.onmousedown = function (e) {
-        e.preventDefault();
-        pos.x = e.clientX;
-        pos.y = e.clientY;
+    function startDrag(clientX, clientY) {
+        pos.x = clientX;
+        pos.y = clientY;
         pos.left = el.offsetLeft;
         pos.top = el.offsetTop;
+    }
+
+    function onDrag(clientX, clientY) {
+        let dx = clientX - pos.x;
+        let dy = clientY - pos.y;
+
+        let newLeft = pos.left + dx;
+        let newTop = pos.top + dy;
+
+        const maxLeft = container.clientWidth - el.clientWidth;
+        const maxTop = container.clientHeight - el.clientHeight;
+
+        newLeft = Math.max(0, Math.min(newLeft, maxLeft));
+        newTop = Math.max(0, Math.min(newTop, maxTop));
+
+        el.style.left = newLeft + "px";
+        el.style.top = newTop + "px";
+    }
+
+    function stopDrag() {
+        document.onmousemove = null;
+        document.onmouseup = null;
+        document.ontouchmove = null;
+        document.ontouchend = null;
+    }
+
+    el.onmousedown = function (e) {
+        e.preventDefault();
+        startDrag(e.clientX, e.clientY);
 
         document.onmousemove = function (e) {
-            let dx = e.clientX - pos.x;
-            let dy = e.clientY - pos.y;
-
-            // Berechne neue Position
-            let newLeft = pos.left + dx;
-            let newTop = pos.top + dy;
-
-            // Begrenzung auf Container
-            const maxLeft = container.clientWidth - el.clientWidth;
-            const maxTop = container.clientHeight - el.clientHeight;
-
-            newLeft = Math.max(0, Math.min(newLeft, maxLeft));
-            newTop = Math.max(0, Math.min(newTop, maxTop));
-
-            el.style.left = newLeft + "px";
-            el.style.top = newTop + "px";
+            onDrag(e.clientX, e.clientY);
         };
+        document.onmouseup = stopDrag;
+    };
 
-        document.onmouseup = function () {
-            document.onmousemove = null;
-            document.onmouseup = null;
-        };
+    el.ontouchstart = function (e) {
+        if (e.touches.length === 1) {
+            startDrag(e.touches[0].clientX, e.touches[0].clientY);
+
+            document.ontouchmove = function (e) {
+                if (e.touches.length === 1) {
+                    onDrag(e.touches[0].clientX, e.touches[0].clientY);
+                }
+            };
+            document.ontouchend = stopDrag;
+        }
     };
 }
 
